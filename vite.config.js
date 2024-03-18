4b367ed1e3499dde022c1daab80d65c9e25fceb6
sam fallowfield <sam.fallowfield1@gmail.com>
2024-03-18 16:47:58 +0000

    Added correct url

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/cicd_pipeline_test",
+  base: "/cicd_pipeline_test/",
 });
